bd28975709ed68c43569cba3af8a09f6dbd938ad
Byron Wasti <byron.wasti@gmail.com>
2014-11-27 11:38:28 -0500

    Merged GetLink and GetHTML because it made no sense having them separate.

--- GetHTML.py
+++ GetHTML.py
@@ -4,6 +4,49 @@ import codecs
 import getpass
 driver = webdriver.Firefox()
 
+USERNAME = str(raw_input("username: "))
+PASSWORD = getpass.getpass("password:  ")
+
+
+def htmlHandle(USERNAME, PASSWORD):
+
+    url = 'https://my.olin.edu/ICS/My_StAR/My_Schedule_and_Registration_Info.jnz'
+
+    viewdetails = "pg0_V_lnkView"
+    driver.get(url)
+    elem = driver.find_element_by_name('userName')
+    elem.send_keys(USERNAME)
+    elem = driver.find_element_by_name('password')
+    elem.send_keys(PASSWORD)
+
+    driver.find_element_by_id("btnLogin").click()
+
+
+    driver.find_element_by_id(viewdetails).click()
+    html = driver.page_source
+
+    soup = BeautifulSoup(html)
+    tabulka = soup.find(id = "pg0_V_ggCourses")
+    tab = tabulka.find('tbody').findAll('tr')[0].findAll('td')[1].string
+
+    Data = []
+
+    for row in tabulka.find('tbody').findAll('tr'):
+        times = []
+        course = str(row.findAll('td')[1].string.strip())
+        for li in row.findAll('td')[2].findAll('li'):
+            times.append(str(li.string.strip()).replace("\r\n\t\t\t\t\t\t\t\t\t\t"," "))
+        Data.append(course)
+        Data.append(times)
+
+    return Data
+
+testfile =open('html.txt','w')
+
+testfile.write(htmlHandle(USERNAME,PASSWORD))
+
+
+'''
 def htmlHandle(USERNAME, PASSWORD):
 
 	url = 'https://my.olin.edu/ICS/My_StAR/My_Schedule_and_Registration_Info.jnz'
@@ -56,7 +99,7 @@ def formatInfo(numOfCourses,courseNames,courseTimes,courseLocations):
 		info += ("\n")
 		info += (courseLocations[i] + "\n")
 	return info
-'''
+
 htmlResults = htmlHandle(USERNAME,PASSWORD)
 
 num = htmlResults[0]
