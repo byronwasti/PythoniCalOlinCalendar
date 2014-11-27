e814908d7d8633bf6b531d82907607cb681872fe
Byron Wasti <byron.wasti@gmail.com>
2014-11-27 11:58:51 -0500

    Now two versions of GetHTML, one using mechanize and one using selenium

--- GetHTML.py
+++ GetHTML.py
@@ -2,7 +2,6 @@ import mechanize
 from bs4 import BeautifulSoup
 import codecs
 import getpass
-driver = webdriver.Firefox()
 
 USERNAME = str(raw_input("username: "))
 PASSWORD = getpass.getpass("password:  ")
@@ -10,48 +9,8 @@ PASSWORD = getpass.getpass("password:  ")
 
 def htmlHandle(USERNAME, PASSWORD):
 
-    url = 'https://my.olin.edu/ICS/My_StAR/My_Schedule_and_Registration_Info.jnz'
-
-    viewdetails = "pg0_V_lnkView"
-    driver.get(url)
-    elem = driver.find_element_by_name('userName')
-    elem.send_keys(USERNAME)
-    elem = driver.find_element_by_name('password')
-    elem.send_keys(PASSWORD)
-
-    driver.find_element_by_id("btnLogin").click()
-
-
-    driver.find_element_by_id(viewdetails).click()
-    html = driver.page_source
-
-    soup = BeautifulSoup(html)
-    tabulka = soup.find(id = "pg0_V_ggCourses")
-    tab = tabulka.find('tbody').findAll('tr')[0].findAll('td')[1].string
-
-    Data = []
-
-    for row in tabulka.find('tbody').findAll('tr'):
-        times = []
-        course = str(row.findAll('td')[1].string.strip())
-        for li in row.findAll('td')[2].findAll('li'):
-            times.append(str(li.string.strip()).replace("\r\n\t\t\t\t\t\t\t\t\t\t"," "))
-        Data.append(course)
-        Data.append(times)
-
-    return Data
-
-testfile =open('html.txt','w')
-
-testfile.write(htmlHandle(USERNAME,PASSWORD))
-
-
-'''
-def htmlHandle(USERNAME, PASSWORD):
-
 	url = 'https://my.olin.edu/ICS/My_StAR/My_Schedule_and_Registration_Info.jnz'
 
-    viewdetails = "pg0_V_lnkView"
 	br = mechanize.Browser()
 	br.open(url)
 
@@ -61,8 +20,6 @@ def htmlHandle(USERNAME, PASSWORD):
 	br["userName"] = USERNAME
 	br["password"] = PASSWORD
 	html = str(br.submit().read())
-    driver.find_element_by_id(viewdetails).click()
-    html = driver.page_source
 
 	soup = BeautifulSoup(html)
 
@@ -106,4 +63,3 @@ num = htmlResults[0]
 names = htmlResults[1]
 times = htmlResults[2]
 locs = htmlResults[3]
-'''
---
+++ GetHTMLSelenium.py
@@ -0,0 +1,49 @@
+#import mechanize
+from bs4 import BeautifulSoup
+import codecs
+import getpass
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+driver = webdriver.Firefox()
+
+USERNAME = str(raw_input("username: "))
+PASSWORD = getpass.getpass("password:  ")
+
+
+def htmlHandle(USERNAME, PASSWORD):
+
+    url = 'https://my.olin.edu/ICS/My_StAR/My_Schedule_and_Registration_Info.jnz'
+
+    viewdetails = "pg0_V_lnkView"
+    driver.get(url)
+    elem = driver.find_element_by_name('userName')
+    elem.send_keys(USERNAME)
+    elem = driver.find_element_by_name('password')
+    elem.send_keys(PASSWORD)
+
+    driver.find_element_by_id("btnLogin").click()
+
+
+    driver.find_element_by_id(viewdetails).click()
+    html = driver.page_source
+
+    soup = BeautifulSoup(html)
+    tabulka = soup.find(id = "pg0_V_ggCourses")
+    tab = tabulka.find('tbody').findAll('tr')[0].findAll('td')[1].string
+
+    Data = []
+
+    for row in tabulka.find('tbody').findAll('tr'):
+        times = []
+        course = str(row.findAll('td')[1].string.strip())
+        for li in row.findAll('td')[2].findAll('li'):
+            times.append(str(li.string.strip()).replace("\r\n\t\t\t\t\t\t\t\t\t\t"," "))
+        Data.append(course)
+        Data.append(times)
+
+    return Data
+
+testfile =open('html.txt','w')
+
+testfile.write(htmlHandle(USERNAME,PASSWORD))
